8b1855a14b4decae86ec9f1cb44329c627d293c8
Bill Davis <bill.davis@gmail.com>
2020-05-31 05:15:33 +0000

    notes changes

--- script.sh
+++ script.sh
@@ -10,8 +10,6 @@ pip3 freeze > requirements.txt
 git remote --verbose
 git remote show origin
 
-git remote add github https://github.com/billdavisjr/task-manager.git
-
 heroku login
 heroku git:remote -a task-manager-bill-davis
 
@@ -19,40 +17,5 @@ git remote set-url --add --push origin https://github.com/billdavisjr/task-manaa
 git remote set-url --add --push origin https://git.heroku.com/task-manager-bill-davis.git
 
 git remote show origin
+
 git remote  --verbose
-# github  https://github.com/billdavisjr/task-manager.git (fetch)
-# github  https://github.com/billdavisjr/task-manager.git (push)
-
-# heroku  https://git.heroku.com/task-manager-bill-davis.git (fetch)
-# heroku  https://git.heroku.com/task-manager-bill-davis.git (push)
-
-# origin  https://github.com/billdavisjr/task-manager.git (fetch)
-# origin  https://github.com/billdavisjr/task-manaager.git (push)
-# origin  https://git.heroku.com/task-manager-bill-davis.git (push)
-
-git remote show origin
-# * remote origin
-#   Fetch URL: https://github.com/billdavisjr/task-manager.git
-#   Push  URL: https://git.heroku.com/task-manager-bill-davis.git
-#   Push  URL: https://github.com/billdavisjr/task-manaager.git
-#   HEAD branch: master
-#   Remote branch:
-#     master tracked
-#   Local branch configured for 'git pull':
-#     master merges with remote master
-#   Local ref configured for 'git push':
-#     master pushes to master (fast-forwardable)
-
-git remote --verbose
-github  https://github.com/billdavisjr/task-manager.git (fetch)
-github  https://github.com/billdavisjr/task-manager.git (push)
-
-heroku  https://git.heroku.com/task-manager-bill-davis.git (fetch)
-heroku  https://git.heroku.com/task-manager-bill-davis.git (push)
-
-origin  https://github.com/billdavisjr/task-manager.git (fetch)
-origin  https://github.com/billdavisjr/task-manaager.git (push)
-
-origin  https://git.heroku.com/task-manager-bill-davis.git (push)
-
-
