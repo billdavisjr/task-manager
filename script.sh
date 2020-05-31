5925043f7dcf2cc648387882bb2328600aafe5ff
Bill Davis <bill.davis@gmail.com>
2020-05-31 05:11:33 +0000

    More notes changes

--- script.sh
+++ script.sh
@@ -9,11 +9,16 @@ pip3 freeze > requirements.txt
 
 git remote --verbose
 git remote show origin
+
 git remote add github https://github.com/billdavisjr/task-manager.git
+
 heroku login
 heroku git:remote -a task-manager-bill-davis
+
 git remote set-url --add --push origin https://github.com/billdavisjr/task-manaager.git
 git remote set-url --add --push origin https://git.heroku.com/task-manager-bill-davis.git
+
+git remote show origin
 git remote  --verbose
 # github  https://github.com/billdavisjr/task-manager.git (fetch)
 # github  https://github.com/billdavisjr/task-manager.git (push)
@@ -28,17 +33,17 @@ git remote  --verbose
 git remote show origin
 # * remote origin
 #   Fetch URL: https://github.com/billdavisjr/task-manager.git
-#   Push  URL: https://github.com/billdavisjr/tash-manaager.git
 #   Push  URL: https://git.heroku.com/task-manager-bill-davis.git
+#   Push  URL: https://github.com/billdavisjr/task-manaager.git
 #   HEAD branch: master
 #   Remote branch:
 #     master tracked
 #   Local branch configured for 'git pull':
 #     master merges with remote master
 #   Local ref configured for 'git push':
-#     master pushes to master (up to date)
+#     master pushes to master (fast-forwardable)
 
-
+git remote --verbose
 github  https://github.com/billdavisjr/task-manager.git (fetch)
 github  https://github.com/billdavisjr/task-manager.git (push)
 
